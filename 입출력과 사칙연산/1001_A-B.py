3f5e458ed45fe6e159b714cd5a2381da7293e89f
Sojin <voise.ksing@gmail.com>
2022-03-09 23:33:51 +0900

    22.03.09

---
+++ 입출력과 사칙연산/1001_A-B.py
@@ -0,0 +1,4 @@
+a = list(map(int, input().split()))
+print(a[0] - a[1])
+
+# 절대값은 abs
---
+++ 입출력과 사칙연산/10869_사칙연산.py
@@ -0,0 +1,6 @@
+a = list(map(int, input().split()))
+print(a[0] + a[1])
+print(a[0] - a[1])
+print(a[0] * a[1])
+print(int(a[0] / a[1]))
+print(a[0] % a[1])
