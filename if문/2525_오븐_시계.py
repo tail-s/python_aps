4c542f2b8b886b7fa76a5dffd413c25651cd8439
Sojin <voise.ksing@gmail.com>
2022-03-11 01:25:52 +0900

    22.03.11

---
+++ if문/2525_오븐_시계.py
@@ -0,0 +1,17 @@
+a, b = [int(x) for x in input().split()]
+c = int(input())
+T = a * 60 + b + c
+a = T // 60
+b = T % 60
+if a >= 24: # c 값이 하루 이상의 값(1440분 이상)이 입력되면 오류
+    a -= 24
+print(a, b)
+
+# a, b = [int(x) for x in input().split()]
+# c = int(input())
+# T = a * 60 + b + c
+# a = T // 60
+# b = T % 60
+# while a >= 24: # c 값에 상관없이 계산 가능
+#     a -= 24
+# print(a, b)
