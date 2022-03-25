8da710c2b293c1a4f2dbd2035ecddecc3c1d3720
Sojin <voise.ksing@gmail.com>
2022-03-26 04:31:12 +0900

    22.03.26 /4948 풀이중/

--- 기본 수학 2/4948_베르트랑 공준.py
+++ 기본 수학 2/4948_베르트랑 공준.py
@@ -19,29 +19,52 @@
 ########################################################
 # 이건 왜안됨?
 
-def solve():
-    is_on = True
-    while is_on:
-        x = int(input())
-        if x == 0:
-            is_on = False
-            pass
-        elif x == 1:
-            print("1")
-        else:
-            m = x + 1
-            n = m * 2
-            primes = [True] * (n + 1)
-            primes[0] = False
-            cnt = 0
-            for i in range(2, n+1):
-                if primes[i]:
-                    for j in range(i*2, n+1, i): # 합성수 찾기 (배수)
-                        primes[j] = False
-            for i in range(m, n+1):
-                if primes[i]:
-                    # print(i)
-                    cnt += 1
-            print(cnt)
-solve()
-# 풀이중
+# def solve():
+#     is_on = True
+#     while is_on:
+#         x = int(input())
+#         if x == 0:
+#             is_on = False
+#             pass
+#         elif x == 1:
+#             print("1")
+#         else:
+#             m = x + 1
+#             n = m * 2
+#             primes = [True] * (n + 1)
+#             primes[0] = False
+#             cnt = 0
+#             for i in range(2, n+1):
+#                 if primes[i]:
+#                     for j in range(i*2, n+1, i): # 합성수 찾기 (배수)
+#                         primes[j] = False
+#             for i in range(m, n+1):
+#                 if primes[i]:
+#                     # print(i)
+#                     cnt += 1
+#             print(cnt)
+# solve()
+# 풀이중 - 실패함 whY?
+
+########################################################
+
+# def sosu(x):
+#     if x == 1:
+#         return False
+#     for i in range(2, int(x ** 0.5) + 1):
+#         if x % i == 0: # 제곱근이 있는 수 중 약수가 있으면 소수 아님
+#             return False
+#     return True # 나머지는 다 소수
+#
+# while True:
+#     n = int(input())
+#     cnt = 0
+#     if n == 0:
+#         break
+#     for i in range(n + 1, (2 * n) + 1):
+#         if sosu(i):
+#             cnt += 1
+#     print(cnt)
+
+# 응 시간제한이야~ 다시해와~
+########################################################
