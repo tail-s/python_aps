9da4b86a3ec11c3473d8522a4c6c93b4efe9921a
Sojin <voise.ksing@gmail.com>
2022-04-04 03:10:58 +0900

    22.04.04 정리

---
+++ 7_기본 수학 1/1193_분수찾기 (풀이중).py
@@ -0,0 +1,16 @@
+# n = int(input())
+# test = 1
+# cnt = 0
+# while n > test:
+#     n -= test
+#     test += 1
+#     cnt += 1
+#
+# if cnt % 2 == 0:
+#     x = n
+#     y = cnt + 1 - x
+# else:
+#     y = n
+#     x = cnt + 1 - y
+#
+# print(f"{x}/{y}") 풀이중
